1f15fc11608d9b91f7797ad879ae7ca3a2887784
rachkindaa <rachkindaa@gmail.com>
2023-10-11 15:11:17 +0300

    all

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+// delegating
+list.addEventListener("click", handleClick);
+let instance = null; 
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `
+    )
+    .join("");
+}
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const bigImUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+    <img
+      src="${bigImUrl}"
+      alt="${event.target.alt}"
+    />`,
+    {
+      onShow: (instance) => {
+        return true;
+      },
+      onClose: (instance) => {
+        return true;
+      },
+    }
+  );
+  instance.show();
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && instance) {
+      instance.close();
+      instance = null;
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `
+    )
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
